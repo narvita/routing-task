24d67aec590cc8a897996569107c30f590dccf40
narvita <narkha21@gmail.com>
2022-08-23 11:28:24 +0400

    created users table and implemented with server

--- routing-front/src/app/user-table/user-table.component.ts
+++ routing-front/src/app/user-table/user-table.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 import { UserInterface } from 'src/interfaces/user.interface';
 import { UsersService } from 'src/services/users.service';
+import { UserDataInterface } from './user.data.ts/user-data.interface';
 
 @Component({
   selector: 'app-user-table',
@@ -11,30 +12,57 @@ import { UsersService } from 'src/services/users.service';
 })
 export class UserTableComponent implements OnInit, OnDestroy{
   public user!: UserInterface;
-  public id = 2;
+  public id!: number;
+  public usersTableItems = [];
+  public usersTabledata: UserDataInterface[] = [];
   private unsubscraber$ = new Subject<null>;
 
 
   constructor(private usersService: UsersService, private activatedRoute: ActivatedRoute) { }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(() => {
       this.id = this.activatedRoute.snapshot.params['id'];
+      // console.log(this.id);
+      
+      this.getUser(this.id);
     });
-    console.log(this.id, 'id');
     
-    this.getUser(this.id);
   }
 
-  public getUser(id: number){
+  public getUser(id: number) {
     this.usersService.getUser(id)
-    .pipe(takeUntil(this.unsubscraber$))
-    .subscribe((data) => this.user = data);
-    console.log(this.user);
-  }
-
-  ngOnDestroy(): void {
-    this.unsubscraber$.next(null);
-    this.unsubscraber$.complete();
-  }
+      .pipe(takeUntil(this.unsubscraber$))
+      .subscribe((data) => {
+        this.user = data;
+        console.log(data);
+        this.usersTabledata = [
+          {
+            label: 'Id',
+            value: data.id,
+          },
+          {
+            label: 'Name',
+            value: data.name,
+          },
+          {
+            label: 'Owner',
+            value: data.owner,
+          },
+          {
+            label: 'Created On',
+            value: data.createdDate,
+          },
+          {
+            label: 'Updated On',
+            value: data.updatedDate,
+          }
+        ]
+      });
+       
+      } 
+      
 }
